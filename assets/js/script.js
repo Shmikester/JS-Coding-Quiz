001795b9056e56ea27c0c88562fa16253e8ebbd1
Heff <shmikester@gmail.com>
2021-12-05 03:22:14 -0700

    more stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,77 @@
 //array for storing/saving scores
 var scoresArray = [];
+var pageLocation = "";
+var timeLeft = "";
 
-loadScores();
+var mainBody = document.querySelector("#mainBody");
+var navSection = document.querySelector("#pageNav");
+var timerCount = document.querySelector("#timer");
+
+initialLoad();
+//loads scores and preQuiz screen
+function initialLoad()
+{
+    setNav();
+    loadScores();
+}
+
+function setNav()
+{
+    navSection.innerHTML = "";
+    timerCount.innerHTML = "";
+    var navBar = document.createElement("div");
+    var timerArea = document.createElement("div");
+    timerArea.innerHTML = "";
+
+    navBar.className = "buttonNav";
+
+    if (pageLocation === "" || pageLocation === "Scores")
+    {
+        pageLocation = "Home";
+        navBar.id = "scoresNav";
+        navBar.innerHTML = `<button type="click">Scores</button>`
+        mainBodyPreQuiz();
+    } else
+    {
+        mainBody.innerHTML = "";
+        pageLocation = "Scores";
+        navBar.id = "homeNav";
+        navBar.innerHTML = `<button type="click">Home</button>`
+    }
+    navSection.appendChild(navBar);
+    timerCount.appendChild(timerArea);
+}
+
+//generates the main body
+function mainBodyPreQuiz()
+{
+
+    var preQuiz = document.createElement("div");
+    preQuiz.className = "preQuiz";
+    preQuiz.innerHTML = `<p>Click the button below to begin</p><button type="click">Start!</button>`;
+    mainBody.appendChild(preQuiz);
+}
+
+function startQuiz()
+{
+    startCountDown(30);
+}
+
+//countdown timer
+function startCountDown(remainingTime)
+{
+    timeLeft = remainingTime;
+    var timerArea = document.createElement("div");
+    timerArea.innerHTML = "";
+    timerCount.appendChild(timerArea);
+
+    var t = setInterval(function ()
+    {
+        timerArea.innerHTML =`<p>Time Left: ${timeLeft}</p>`;
+        timerCount.appendChild(timerArea);
+    }, 1000);
+    
+}
 
 // load in saved scores, if any exist
 function loadScores()
@@ -35,3 +105,7 @@ function showScores(scoresArray)
 {
 
 }
+
+//button even handlers
+mainBody.addEventListener("click", startQuiz);
+navSection.addEventListener("click", setNav);
